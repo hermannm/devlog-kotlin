599e276ae542ee7afebaec375909d6cfc68ee87f
hermannm <hermann.morkrid@gmail.com>
2024-12-08 20:39:03 +0100

    Implement rawMarker function for creating log markers from JSON

--- src/main/kotlin/dev/hermannm/devlog/LogMarker.kt
+++ src/main/kotlin/dev/hermannm/devlog/LogMarker.kt
@@ -7,6 +7,7 @@ import java.time.Instant
 import java.util.*
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
 import net.logstash.logback.marker.Markers
 import org.slf4j.Marker as Slf4jMarker
 
@@ -44,10 +45,39 @@ inline fun <reified ValueT> marker(name: String, value: ValueT): LogMarker {
       } catch (_: Exception) {
         // We don't want to ever throw an exception from constructing a marker, which may happen if
         // serialization fails, for example. So in those cases we fall back to toString().
-        Markers.append("name", value.toString())
+        Markers.append(name, value.toString())
       }
 
   return LogMarker(slf4jMarker)
+}
+
+fun rawMarker(name: String, json: String, validJson: Boolean = false): LogMarker {
+  try {
+    // Some log platforms (e.g. AWS CloudWatch) use newlines as the separator between log messages.
+    // So if the JSON string has unescaped newlines, we must re-parse the JSON.
+    val containsNewlines = json.contains('\n')
+
+    // If we assume the JSON is valid, and there are no unescaped newlines, we can return it as-is.
+    if (validJson && !containsNewlines) {
+      return LogMarker(Markers.appendRaw(name, json))
+    }
+
+    // If we do not assume that the JSON is valid, we must try to decode it.
+    val decoded = markerJson.parseToJsonElement(json)
+
+    // If we successfully decoded the JSON, and it does not contain unescaped newlines, we can
+    // return it as-is.
+    if (!containsNewlines) {
+      return LogMarker(Markers.appendRaw(name, json))
+    }
+
+    // If the JSON did contain unescaped newlines, then we need to re-encode to escape them.
+    val encoded = markerJson.encodeToString(JsonElement.serializer(), decoded)
+    return LogMarker(Markers.appendRaw(name, encoded))
+  } catch (_: Exception) {
+    // If we failed to decode/re-encode the JSON string, we return it as a non-JSON string.
+    return LogMarker(Markers.append(name, json))
+  }
 }
 
 @PublishedApi // PublishedApi so we can use this in the inline `marker` function.
