52ea61bbd84a9682c2c3180fdb63315fa1bc9808
hermannm <hermann.morkrid@gmail.com>
2025-06-09 19:35:55 +0200

    Release v0.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 group = "dev.hermannm"
 
-version = "0.6.0"
+version = "0.6.1"
 
 mavenPublishing {
   coordinates(group.toString(), artifactId = "devlog-kotlin", version.toString())
